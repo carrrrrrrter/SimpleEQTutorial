efd105bb6376a58807045e6ac4ecc456222f6f22
carterfodge <carterfodge99@gmail.com>
2023-05-25 10:55:49 -0400

    Initial Modifications
    Start of building EQ framework, simplifying peak gain/Q code

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,6 +10,21 @@
 
 #include <JuceHeader.h>
 
+enum Slope {
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
+struct ChainSettings {
+    float peakFreq{0}, peakGainInDecibels{0}, peakQuality{1.f};
+    float lowCutFreq{0}, highCutFreq{0};
+    Slope lowCutSlope{Slope::Slope_12}, highCutSlope{Slope::Slope_12};
+};
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
 //==============================================================================
 /**
 */
@@ -58,8 +73,31 @@ public:
     
     static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
     juce::AudioProcessorValueTreeState apvts {*this, nullptr, "Parameters", createParameterLayout()};
+    
+    
 
 private:
+    
+    using Filter = juce::dsp::IIR::Filter<float>;
+    
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    
+    MonoChain leftChain, rightChain;
+    
+    enum ChainPositions {
+        LowCut,
+        Peak,
+        HighCut
+    };
+    
+    
+    void updatePeakFilter(const ChainSettings& chainSettings);
+    
+    using Coefficients = Filter::CoefficientsPtr;
+    
+    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SImpleEQAudioProcessor)
 };
